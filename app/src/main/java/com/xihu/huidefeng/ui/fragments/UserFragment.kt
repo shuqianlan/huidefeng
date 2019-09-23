fe41d2dd884948ea95903cfaf68e6ba561420548
吴正华 <351563763@qq.com>
2019-09-23 18:02:58 +0800

    - sync
    同步

--- app/src/main/java/com/xihu/huidefeng/ui/fragments/UserFragment.kt
+++ app/src/main/java/com/xihu/huidefeng/ui/fragments/UserFragment.kt
@@ -36,16 +36,16 @@ class UserFragment : BaseFragment() {
 	override fun initView(v:View) {
 		datas.clear()
 		datas.apply {
-			add(ItemBean(getString(R.string.user_recharge), R.drawable.recharge, category = 2))
-			add(ItemBean(getString(R.string.user_withdraw), R.drawable.withdraw, category = 5))
-			add(ItemBean(getString(R.string.user_balance), R.drawable.balance, category = 3))
-			add(ItemBean(getString(R.string.user_score), R.drawable.score, category = 4))
+			add(ItemBean(getString(R.string.user_recharge), R.drawable.recharge, category = 1))
+			add(ItemBean(getString(R.string.user_withdraw), R.drawable.withdraw, category = 2))
+//			add(ItemBean(getString(R.string.user_balance), R.drawable.balance, category = 3))
+			add(ItemBean(getString(R.string.user_score), R.drawable.score, category = 3))
 //			add(ItemBean(getString(R.string.user_atlas), R.drawable.vip_atlas, category = 6))
-			add(ItemBean(getString(R.string.user_notice), R.drawable.vip_notice, category = 7))
-			add(ItemBean(getString(R.string.user_security), R.drawable.security, category = 8))
-			add(ItemBean(getString(R.string.user_info), R.drawable.user_info, category = 9))
-			add(ItemBean(getString(R.string.user_download), R.drawable.download, category = 10))
-			add(ItemBean(getString(R.string.user_logout), R.drawable.logout, category = 11))
+			add(ItemBean(getString(R.string.user_notice), R.drawable.vip_notice, category = 4))
+			add(ItemBean(getString(R.string.user_security), R.drawable.security, category = 5))
+			add(ItemBean(getString(R.string.user_info), R.drawable.user_info, category = 6))
+			add(ItemBean(getString(R.string.user_download), R.drawable.download, category = 7))
+			add(ItemBean(getString(R.string.user_logout), R.drawable.logout, category = 8))
 		}
 
 		user_items.adapter = KotlinDataAdapter.Builder<ItemBean, LayoutUserItemHolderBinding>().apply {
@@ -60,16 +60,16 @@ class UserFragment : BaseFragment() {
 			setData(datas)
 			onItemClick() {
 				view, bean -> when(bean.category) {
-					2 -> navigateTo(view, R.id.action_user_fragment_to_charge_fragment, R.string.user_recharge)
-					3 -> navigateTo(view, R.id.action_user_fragment_to_balance_fragment, R.string.user_balance)
-					4 -> navigateTo(view, R.id.action_user_fragment_to_score_fragment, R.string.user_score)
-					5 -> navigateTo(view, R.id.action_user_fragment_to_withdraw_fragment, R.string.user_withdraw)
-					6 -> navigateTo(view, R.id.action_user_fragment_to_atlas_fragment, R.string.user_atlas)
-					7 -> navigateTo(view, R.id.action_user_fragment_to_notice_fragment, R.string.user_notice)
-					8 -> navigateTo(view, R.id.action_user_fragment_to_security_fragment, R.string.user_security)
-					9 -> navigateTo(view, R.id.action_user_fragment_to_info_fragment, R.string.user_info)
-					10 -> navigateTo(view, R.id.action_user_fragment_to_download_fragment, R.string.user_download)
-					11 -> onAlertExitApp(view)
+					1 -> navigateTo(view, R.id.action_user_fragment_to_charge_fragment, R.string.user_recharge)
+					2 -> navigateTo(view, R.id.action_user_fragment_to_withdraw_fragment, R.string.user_withdraw)
+//					3 -> navigateTo(view, R.id.action_user_fragment_to_balance_fragment, R.string.user_balance)
+					3 -> navigateTo(view, R.id.action_user_fragment_to_score_fragment, R.string.user_score)
+//					6 -> navigateTo(view, R.id.action_user_fragment_to_atlas_fragment, R.string.user_atlas)
+					4 -> navigateTo(view, R.id.action_user_fragment_to_notice_fragment, R.string.user_notice)
+					5 -> navigateTo(view, R.id.action_user_fragment_to_security_fragment, R.string.user_security)
+					6 -> navigateTo(view, R.id.action_user_fragment_to_info_fragment, R.string.user_info)
+					7 -> navigateTo(view, R.id.action_user_fragment_to_download_fragment, R.string.user_download)
+					8 -> onAlertExitApp(view)
 				}
 			}
 			itemViewType { b -> b.viewType }
